d000537e147783eef3a9aaa23e5b7c5572fde337
NEELAGIRIDEEPAK <deepakdeepu27451@gmail.com>
2022-12-21 20:32:55 +0530

    Update Primenumber.java

--- Assignment1/src/com/Assignment1/Primenumber.java
+++ Assignment1/src/com/Assignment1/Primenumber.java
@@ -2,18 +2,6 @@ package com.Assignment1;
 
 public class Primenumber {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num = 3;
-		int count = 0;
-		for (int i = 1; i <= num; i++) {
-			if (num % i == 0) {
-				count++;
-			}
-		}
-		if (count > 2) {
-			System.out.println("3 is not a prime number");
-		} else {
-			System.out.println("3 is a prime number");
-		}
+		System.out.println("given number is prime or not");
 	}
 }
