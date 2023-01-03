e145d9425498e8c73ec7fc9f4d73ce9a5d72b513
NEELAGIRIDEEPAK <deepakdeepu27451@gmail.com>
2023-01-03 21:54:38 +0530

    coomit7

---
+++ Assignment1/src/com/Exception/ExceptionHandling2.java
@@ -0,0 +1,52 @@
+package com.Exception;
+
+import java.util.Scanner;
+
+public class ExceptionHandling2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Scanner sc= new Scanner(System.in);
+			System.out.println("enter a number to check even");
+			int num=sc.nextInt();
+			try {
+				if(num % 2 == 0) {
+					System.out.println("yes");
+				}
+				else {
+					System.out.println("no");
+				}
+			}
+			catch(Exception e){
+					System.out.println("i am an exception");
+				}
+			System.out.println("enter a number to check odd");
+			int num1=sc.nextInt();
+			try {
+				if(num1 % 2 == 1) {
+					System.out.println("yes");
+				}
+				else {
+					System.out.println("no");
+				}
+			}
+			catch(Exception e){
+					System.out.println("i am an exception");
+				}
+			if(num / 1 != 0) {
+				System.out.println("it is a number");
+			}
+			if(num1 / 1 != 0) {
+				System.out.println("it is a number");
+			}
+			else {
+				System.out.println("it is an alphabet");
+			}
+		}
+		catch(Exception e){
+			System.out.println("i am an exception");
+		}
+	}
+
+}
